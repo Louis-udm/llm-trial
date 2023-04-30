f43f88e44753cf666a6a5168da9bd1e8c0a91d6e
Zhibin Lu <zhibin.lu@siainnovations.com>
2023-04-30 02:46:10 +0000

    modify train_cola.py

--- trials/vgcn_bert/train_cola.py
+++ trials/vgcn_bert/train_cola.py
@@ -20,14 +20,14 @@ from transformers.modeling_outputs import SequenceClassifierOutput
 from transformers.models.vgcn_bert.modeling_graph import WordGraph,_normalize_adj
 
 parser = argparse.ArgumentParser()
-# parser.add_argument('--ds', type=str, default='mr')
-# parser.add_argument('--load', type=int, default=0)
-# parser.add_argument('--sw', type=int, default='0')
-# parser.add_argument('--dim', type=int, default='16')
-parser.add_argument('--lr', type=float, default=5e-5)
-parser.add_argument('--l2', type=float, default=0.001)
-parser.add_argument('--model', type=str, default='vb')
-# parser.add_argument('--model', type=str, default='distilbert')
+# parser.add_argument('--ds', type = str, default='mr')
+# parser.add_argument('--load', type = int, default=0)
+# parser.add_argument('--sw', type = int, default='0')
+# parser.add_argument('--dim', type = int, default='16')
+parser.add_argument('--lr', type = float, default=5e-5)
+parser.add_argument('--ep', type = int, default=15)
+parser.add_argument('--l2', type = float, default=0.001)
+parser.add_argument('--model', type = str, default='vb') # vb: vgcn_bert, db: distilbert
 args = parser.parse_args()
 
 print(f"\nStart Datetime: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
@@ -49,7 +49,11 @@ print(f"Random Seed: {seed}, Device: {device}")
 os.environ["TRANSFORMERS_OFFLINE"] = "1"
 
 # specify path to local model files
-model_path = "/tmp/local-huggingface-models/zhibinlu_vgcn-distilbert-base-uncased" if args.model=='vb' else "/tmp/local-huggingface-models/hf-maintainers_distilbert-base-uncased"
+model_path = (
+    "/tmp/local-huggingface-models/zhibinlu_vgcn-distilbert-base-uncased" if args.model=='vb' 
+    else "/tmp/local-huggingface-models/hf-maintainers_distilbert-base-uncased"
+)
+              
 
 
 print(f"Model Path: {model_path} \nModel config:")
@@ -73,15 +77,15 @@ print(f"Valid Data Taux: {valid_data_taux}, Test Data Taux: {test_data_taux}")
 label2idx = {"0": 0, "1": 1}
 idx2label = {0: "0", 1: "1"}
 train_valid_df = pd.read_csv(
-    os.path.join(ds_path, "train.tsv"), encoding="utf-8", header=None, sep="\t"
+    os.path.join(ds_path, "train.tsv"), encoding="utf-8", header = None, sep="\t"
 )
-train_valid_df=train_valid_df[[0,1,3]] # 0-ref, 1-label, 3-sentence
+train_valid_df = train_valid_df[[0,1,3]] # 0-ref, 1-label, 3-sentence
 train_valid_df = shuffle(train_valid_df)
 # use dev set as test set, because we can not get the ground true label of the real test set.
 test_df = pd.read_csv(
-    os.path.join(ds_path, "dev.tsv"), encoding="utf-8", header=None, sep="\t"
+    os.path.join(ds_path, "dev.tsv"), encoding="utf-8", header = None, sep="\t"
 )
-test_df=test_df[[0,1,3]]
+test_df = test_df[[0,1,3]]
 
 train_valid_size = train_valid_df[1].count()
 valid_size = int(train_valid_size * valid_data_taux)
@@ -104,7 +108,7 @@ y_prob_test = np.eye(len(y_test), len(label2idx))[y_test]
 Prepare dataset
 """
 class ColaDataset(Dataset):
-    def __init__(self, corpus, y, y_prob, tokenizer,max_len=512):
+    def __init__(self, corpus, y, y_prob, tokenizer, max_len=512):
         self.corpus = corpus
         self.y = y
         self.y_prob = y_prob
@@ -122,32 +126,33 @@ class ColaDataset(Dataset):
         y_prob = self.y_prob[idx]
         # https://huggingface.co/transformers/v3.5.1/internal/tokenization_utils.html#transformers.tokenization_utils_base.PreTrainedTokenizerBase.encode_plus
         # inputs = self.tokenizer.encode_plus(
-        #     text=sentence,
-        #     text_pair=None,
-        #     add_special_tokens=True,
-        #     max_length=self.max_len,
-        #     # pad_to_max_length=True,
+        #     text = sentence,
+        #     text_pair = None,
+        #     add_special_tokens = True,
+        #     max_length = self.max_len,
+        #     # pad_to_max_length = True,
         #     padding="max_length",
         #     # padding="longest",
-        #     # return_token_type_ids=True,
-        #     truncation=True,
+        #     # return_token_type_ids = True,
+        #     truncation = True,
         #     return_tensors="pt",
         # )
         inputs = self.tokenizer(
-            text=sentence,
-            text_pair=None,
+            text = sentence,
+            text_pair = None,
             padding="max_length", 
-            max_length=self.max_len, 
-            truncation=True,
+            max_length = self.max_len, 
+            truncation = True,
             return_tensors="pt",
         )
-        inputs={k:v[0] for k,v in inputs.items()}
-        return inputs,y,y_prob
+        inputs={k:v[0] for k, v in inputs.items()}
+        return inputs, y, y_prob
     
 MAX_LEN = 512-16
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 32
-TOTAL_EPOCH = 9
+# TOTAL_EPOCH = 9
+TOTAL_EPOCH = args.ep
 # LEARNING_RATE = 5e-5 #1e-05 # 2e-5, 5e-5, old 8e-6
 LEARNING_RATE = args.lr
 # L2_DECAY = 0.001
@@ -157,29 +162,29 @@ L2_DECAY = args.l2
 print(f"\nTraining parameters: MAX_LEN: {MAX_LEN}, TRAIN_BATCH_SIZE: {TRAIN_BATCH_SIZE}, VALID_BATCH_SIZE: {VALID_BATCH_SIZE}, TOTAL_EPOCH: {TOTAL_EPOCH}, LEARNING_RATE: {LEARNING_RATE}, L2_DECAY: {L2_DECAY}")
 
 train_dataloader = DataLoader(
-    dataset=ColaDataset(
+    dataset = ColaDataset(
         train_valid_df[3][:train_size], 
         y_train_valid[:train_size], 
         y_prob_train_valid[:train_size], 
         tokenizer,
         MAX_LEN
-    ), batch_size=TRAIN_BATCH_SIZE, shuffle=False)
+    ), batch_size = TRAIN_BATCH_SIZE, shuffle = False)
 valid_dataloader = DataLoader(
-    dataset=ColaDataset(
+    dataset = ColaDataset(
         train_valid_df[3][train_size:], 
         y_train_valid[train_size:], 
         y_prob_train_valid[train_size:], 
         tokenizer,
         MAX_LEN
-    ), batch_size=VALID_BATCH_SIZE, shuffle=False)
+    ), batch_size = VALID_BATCH_SIZE, shuffle = False)
 test_dataloader = DataLoader(
-    dataset=ColaDataset(
+    dataset = ColaDataset(
         test_df[3], 
         y_test, 
         y_prob_test, 
         tokenizer,
         MAX_LEN
-    ), batch_size=VALID_BATCH_SIZE, shuffle=False)
+    ), batch_size = VALID_BATCH_SIZE, shuffle = False)
 
 """
 build/Load wgraph
@@ -193,11 +198,16 @@ if args.model=="vb":
             wgraph = dill.load(f)
     else:
         window_size=1000
-        remove_stopwords=False
+        remove_stopwords = False
         print(f"Build Word Graph, winidow_size={window_size}, remove_stopwords={remove_stopwords}")
-        wgraph=WordGraph(rows=train_valid_df[3], tokenizer=tokenizer,window_size=window_size, remove_stopwords=remove_stopwords)
+        wgraph = WordGraph(
+            rows = train_valid_df[3], 
+            tokenizer = tokenizer,
+            window_size = window_size, 
+            remove_stopwords = remove_stopwords
+        )
         with open(cola_wgraph_path, "wb") as f:
-            dill.dump(wgraph,f)
+            dill.dump(wgraph, f)
 
     torch_graph = wgraph.to_torch_sparse()
 
@@ -231,13 +241,24 @@ else:
     model = tfr.AutoModelForSequenceClassification.from_pretrained(model_path)
     # model = tfr.AutoModel.from_pretrained(model_path)
 
-if isinstance(model,tfr.VGCNBertForSequenceClassification):
+if isinstance(model, tfr.VGCNBertForSequenceClassification):
     print(f"\nVGCN weights transparant setting:{(model.vgcn_bert.embeddings.vgcn.W_vh_list[0]==1).all()}")
     print(f"VGCN fc transparant setting:{(model.vgcn_bert.embeddings.vgcn.fc_hg.weight==1).all()}")
 
 model.to(device)
 # model.vgcn_bert.embeddings.vgcn.set_transparent_parameters()
 
+# get GPU memory usage in Mb for the current device
+def get_gpu_memory_usage():
+    return max([torch.cuda.memory_allocated(),torch.cuda.max_memory_allocated(), torch.cuda.memory_reserved()]) / 1024 ** 2
+
+# calculate the number of trainable parameters in the model
+def get_num_params(model):
+    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return num_params
+
+print(f"\nGPU memory usage: {get_gpu_memory_usage():.2f} MB")
+print(f"Number of trainable parameters: {get_num_params(model):,}")
 
 """
 Train
@@ -251,9 +272,9 @@ def evaluate(model, dataloader):
     for batch in dataloader:
         inputs, y, y_prob = batch
         inputs = {k: v.to(device) for k, v in inputs.items()}
-        y=y.to(device)
+        y = y.to(device)
         with torch.no_grad():
-            outputs:SequenceClassifierOutput = model(**inputs, labels=y)
+            outputs:SequenceClassifierOutput = model(**inputs, labels = y)
             # loss, logits, hidden_states, attentions = outputs
         # logits = outputs.logits.detach().cpu().numpy()
         # label_ids = y.to("cpu").numpy()
@@ -280,8 +301,8 @@ def train(model, train_dataloader, valid_dataloader, test_dataloader, optimizer)
         for step, batch in enumerate(train_dataloader):
             inputs, y_train, y_prob_train = batch
             inputs = {k: v.to(device) for k, v in inputs.items()}
-            y_train=y_train.to(device)
-            outputs:SequenceClassifierOutput = model(**inputs, labels=y_train)
+            y_train = y_train.to(device)
+            outputs:SequenceClassifierOutput = model(**inputs, labels = y_train)
             # loss, logits, hidden_states, attentions = outputs
             outputs.loss.backward()
             optimizer.step()
@@ -296,15 +317,15 @@ def train(model, train_dataloader, valid_dataloader, test_dataloader, optimizer)
         avg_train_loss = total_loss / len_train_ds
         evaluate_t0=time.time()
         y_preds, y, avg_valid_loss = evaluate(model, valid_dataloader)
-        valid_f1=f1_score(y,y_preds,average="weighted")
+        valid_f1=f1_score(y, y_preds, average="weighted")
         print(f"---------------------------Epoch {epoch} finished----------------------------")
         print(f"  Avg training loss: {avg_train_loss:.6f}, Avg valid loss: {avg_valid_loss:.6f}")
-        print(f"  Avg valid F1: {valid_f1:.5f}, Valid evaluation took: {(time.time() - evaluate_t0)/60.0:.2f}m")
+        print(f"  Valid weighted F1: {valid_f1:.5f}, Valid evaluation took: {(time.time() - evaluate_t0)/60.0:.2f}m")
 
         test_t0=time.time()
         y_preds, y, avg_test_loss = evaluate(model, test_dataloader)
-        test_f1=f1_score(y,y_preds,average="weighted")
-        print(f"  Avg test F1: {test_f1:.5f}, Avg test loss: {avg_test_loss:.6f}, Test evaluation took: {(time.time() - test_t0)/60.0:.2f}m")
+        test_f1=f1_score(y, y_preds, average="weighted")
+        print(f"  Test weighted F1: {test_f1:.5f}, Avg test loss: {avg_test_loss:.6f}, Test evaluation took: {(time.time() - test_t0)/60.0:.2f}m")
         
         all_loss_list["train"].append(avg_train_loss)
         all_loss_list["valid"].append(avg_valid_loss)
@@ -312,20 +333,21 @@ def train(model, train_dataloader, valid_dataloader, test_dataloader, optimizer)
         all_f1_list["valid"].append(valid_f1)
         all_f1_list["test"].append(test_f1)
         # break
+    return all_loss_list, all_f1_list
 
-    best_valid_f1 = max(all_f1_list["valid"])
-    idx_best_valid_f1 = all_f1_list["valid"].index(best_valid_f1)
-    print(f"\n**Optimization Finished!,Total spend: {(time.time() - train_start)/60.0:0.2f}m")
-    print("**Best Valid weighted F1: %.3f at %d epoch."% (100 * best_valid_f1, idx_best_valid_f1))
-    print("**Test weighted F1 when valid best: %.3f" % (100 * all_f1_list["test"][idx_best_valid_f1]))
+train_start = time.time()
+optimizer = AdamW(model.parameters(), lr = LEARNING_RATE, weight_decay = L2_DECAY)
+# optimizer = SparseAdam(model.parameters(), lr = LEARNING_RATE, eps=1e-8)
+print(f"\nTraining start Datetime: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n")
+all_loss_list, all_f1_list = train(model, train_dataloader, valid_dataloader, test_dataloader, optimizer)
+print(f"\nGPU memory usage: {get_gpu_memory_usage():.2f} MB")
+print(f"\nTraining End Datetime: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n")
 
-
-
-optimizer = AdamW(model.parameters(), lr=LEARNING_RATE, weight_decay=L2_DECAY)
-# optimizer = SparseAdam(model.parameters(), lr=LEARNING_RATE, eps=1e-8)
-print(f"\nTring start Datetime: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n")
-train(model, train_dataloader, valid_dataloader, test_dataloader, optimizer)
-print(f"\nTring End Datetime: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}\n")
+best_valid_f1 = max(all_f1_list["valid"])
+idx_best_valid_f1 = all_f1_list["valid"].index(best_valid_f1)
+print(f"\nOptimization Finished! Total spend: {(time.time() - train_start)/60.0:0.2f}m")
+print("Final best valid weighted F1: %.3f at %d epoch."% (100 * best_valid_f1, idx_best_valid_f1))
+print("Final test weighted F1 when best valid: %.3f" % (100 * all_f1_list["test"][idx_best_valid_f1]))
 
 """
 Test
@@ -340,16 +362,16 @@ def try_examples():
     # randomly select 10 samples from test_df
     sample_indices = np.random.randint(0, len(test_df), 10)
     sample_sentences = test_df.iloc[sample_indices][3].tolist()
-    sample_y=y_test[sample_indices]
+    sample_y = y_test[sample_indices]
 
 
     sample_inputs = tokenizer(
         sample_sentences,
-        max_length=MAX_LEN,
+        max_length = MAX_LEN,
         # padding="max_length",
         # padding="do_not_pad",
-        padding=True,
-        truncation=True,
+        padding = True,
+        truncation = True,
         return_tensors="pt",
     )
     # print(sample_inputs)
@@ -363,9 +385,9 @@ def try_examples():
     sample_inputs = {k: v.to(device) for k, v in sample_inputs.items()}
     outputs = model(**sample_inputs)
     predictions = outputs.logits.argmax(dim=1)
-    print("\n".join([f"p:{p} - y:{y}  {s}" for p,y,s in zip(predictions,sample_y,sample_sentences)]))
+    print("\n".join([f"p:{p} - y:{y}  {s}" for p, y, s in zip(predictions, sample_y, sample_sentences)]))
 
-try_examples()
+# try_examples()
 
 """
 save weights
